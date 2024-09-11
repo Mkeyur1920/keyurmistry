05560f54d71e3bc1da42f5cc7b25dca7da706d3e
Mkeyur1920 <keyurmistry19@gmail.com>
2024-09-11 22:43:23 +0530

    profile-picture commit.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Projects.css";
 
 const Projects = () => {
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    // Trigger the message to show after a delay
+    setTimeout(() => {
+      setShowMessage(true);
+    }, 500); // Adjust delay as needed (500ms here)
+  }, []);
+
   return (
     <div className="PojectsClass">
-      <h1>My Projects</h1>
-      <p>Page on working...::) stay tune with me !!</p>
+      <div className="loading-container">
+        {showMessage && (
+          <h1 className="loading-text">
+            Page is working... Stay tuned with me ! :) ⚒️
+          </h1>
+        )}
+      </div>
+      {/* <h1>My Projects</h1>
+      <p>Page on working...::) stay tune with me !!</p> */}
       {/* <p>Here are some of the projects I've worked on:</p>
       <ul>
         <li>Project 1</li>
